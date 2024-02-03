8d23e602a6a18653ef21ffb6f79a46da6570c54f
Shahid Ali <beingshahidali@gmail.com>
2024-02-03 21:49:19 +0530

    Logic code added

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.unitconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mybtn= findViewById(R.id.mybtn);
+        TextView title = findViewById(R.id.textView);
+        TextView weight = findViewById(R.id.textView2);
+        EditText editText = findViewById(R.id.editText);
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value = (Integer.parseInt(editText.getText().toString()));
+                double finalAns = value*2.20;
+
+                title.setText("Your weight is ");
+                weight.setText(String.format("%.2f", finalAns));
+
+
+
+            }
+        });
     }
 }
